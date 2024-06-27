26364aff137865afda1cb923a449754afa6fbdc3
tarikwaleed <tarikwaleed.tech@gmail.com>
2024-06-27 05:54:32 +0300

    changes

---
+++ js/arrow-functions.js
@@ -0,0 +1,11 @@
+/*
+ * here is multiple ways you can write functions
+ * */
+
+function foo(input) {
+  return input + 2
+}
+const bar = (input) => { return input + 2 }
+const baz = (input) => input + 2
+const foobar = input => input + 2
+console.log(baz(2))
--- js/higher-order-functions/index.js
+++ js/higher-order-functions/index.js
@@ -1,25 +1,25 @@
 function copyArrayAndManipulate(array, callback) {
-    const output = []
-    for (let index = 0; index < array.length; index++) {
-        output.push(callback(array[index]))
-    }
-    return output
+  const output = []
+  for (let index = 0; index < array.length; index++) {
+    output.push(callback(array[index]))
+  }
+  return output
 }
 // or, more simply ..
 function copyArrayAndManipulate(array, callback) {
-    return array.map(callback);
+  return array.map(callback);
 }
 // or
 function copyArrayAndManipulate(array, callback) {
-    const output = [];
-    array.forEach(element => {
-        output.push(callback(element));
-    });
-    return output;
+  const output = [];
+  array.forEach(element => {
+    output.push(callback(element));
+  });
+  return output;
 }
 
 function mulBy2(input) {
-    return input * 2
+  return input * 2
 }
 const result = copyArrayAndManipulate([2, 3, 4], mulBy2)
 console.log(result)
